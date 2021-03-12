3c4f7fa0536c59d6c31d2e717e7801c00ee35a92
Mitchell <mitchm1107@yahoo.com>
2021-03-12 23:05:39 +0200

    set up firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAQ8E2zm8R8S2OTx0SqIkp9FKpLltzOfMs',
+    authDomain: 'firestarter-e49c8.firebaseapp.com',
+    projectId: 'firestarter-e49c8',
+    storageBucket: 'firestarter-e49c8.appspot.com',
+    messagingSenderId: '105444938854',
+    appId: '1:105444938854:web:cc8aef7d5ee2b55ad209a7',
+    measurementId: 'G-BHE6Z4KSXM',
+  },
 };
